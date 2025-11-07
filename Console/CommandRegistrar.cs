f50a11958f4bd6d269708ba71105e658231ef93a
H1je <moraniec.krzysztof@gmail.com>
2025-11-07 18:37:28 +0100

    Add Random Number Generator (#1)
    Co-authored-by: moran <moran@KRZYSZTOF>

--- Console/CommandRegistrar.cs
+++ Console/CommandRegistrar.cs
@@ -1,11 +1,12 @@
-﻿using System.CommandLine;
+using System.CommandLine;
 using DevTools.Console.Commands.HelloCommand;
-
+using DevTools.Console.Commands.NumberCommand;
 namespace DevTools.Console;
 internal static class CommandRegistrar
 {
     public static void RegisterCommands(this RootCommand rootCommand)
     {
         rootCommand.Add(new HelloCommand());
+        rootCommand.Add(new NumberCommand());
     }
 }
