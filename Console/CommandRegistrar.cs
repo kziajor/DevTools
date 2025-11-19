ae7a5ff0b1129f918f125c05bbbc3b5c8bfa0b25
bartoszwilk07 <bartoszwilk07@gmail.com>
2025-11-19 16:34:04 +0100

    GENCommand (#5)
    Co-authored-by: barto <barto@DESKTOP-96IM3E5>

--- Console/CommandRegistrar.cs
+++ Console/CommandRegistrar.cs
@@ -1,10 +1,7 @@
 using System.CommandLine;
 
-using DevTools.Console.Commands.EmailCommand;
-using DevTools.Console.Commands.NIPCommand;
+using DevTools.Console.Commands.GenCommand;
 using DevTools.Console.Commands.HelloCommand;
-using DevTools.Console.Commands.NumberCommand;
-using DevTools.Console.Commands.RegonCommand;
 
 namespace DevTools.Console;
 internal static class CommandRegistrar
@@ -12,9 +9,6 @@ internal static class CommandRegistrar
     public static void RegisterCommands(this RootCommand rootCommand)
     {
         rootCommand.Add(new HelloCommand());
-        rootCommand.Add(new EmailCommand());
-        rootCommand.Add(new NIPCommand());
-        rootCommand.Add(new NumberCommand());
-        rootCommand.Add(new RegonCommand());
+        rootCommand.Add(new GenCommand());
     }
 }
